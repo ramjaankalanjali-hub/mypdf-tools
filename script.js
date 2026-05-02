199a576b0c70ecde2e9b5bbcc93efea9cb82a21f
ramjaankalanjali-hub <ramjaankalanjali@gmail.com>
2026-05-02 15:26:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-function showMessage() {
+async function mergePDF() {
+
+    const { PDFDocument } = PDFLib;
 
     let fileInput = document.getElementById("pdfFile");
 
-    if(fileInput.files.length > 0){
+    if(fileInput.files.length < 2){
 
-        document.getElementById("fileName").innerText =
-        fileInput.files[0].name;
+        alert("Select at least 2 PDF files");
 
-    } else {
-
-        alert("Please Select PDF File");
+        return;
 
     }
 
-}
+    const mergedPdf = await PDFDocument.create();
 
-async function mergePDF() {
+    for(let i = 0; i < fileInput.files.length; i++){
 
-    alert("Merge PDF Button Working");
+        const file = fileInput.files[i];
+
+        const bytes = await file.arrayBuffer();
+
+        const pdf = await PDFDocument.load(bytes);
+
+        const copiedPages = await mergedPdf.copyPages(
+            pdf,
+            pdf.getPageIndices()
+        );
+
+        copiedPages.forEach((page) => {
+
+            mergedPdf.addPage(page);
+
+        });
+
+    }
+
+    const mergedBytes = await mergedPdf.save();
+
+    const blob = new Blob([mergedBytes], {
+        type: "application/pdf"
+    });
+
+    const link = document.createElement("a");
+
+    link.href = URL.createObjectURL(blob);
+
+    link.download = "merged.pdf";
+
+    link.click();
 
 }
